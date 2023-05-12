aa8451887c48dfb2d1480edf069d0a56cb16645c
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-05-12 15:45:23 +0530

    allow gyanl.com only

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://gyanl.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
